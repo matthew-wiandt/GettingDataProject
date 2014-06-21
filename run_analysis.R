3ccaf8de6d0f8946753ee128cef3b50e79e8f474
Matthew Wiandt <healthsystem@users.noreply.github.com>
2014-06-21 16:10:41 -0500

    corrected error in merge that resulted in mixing up rows

--- run_analysis.R
+++ run_analysis.R
@@ -37,40 +37,39 @@ if(!file.exists("./UCI HAR Dataset"))
     columns <- grep("mean\\()|std\\()",var_lower)
     data2 <- data[,columns]    
 
-    #Combine the training and test data and
-    #add columns which identify subject and activities
+    #Combine the activities identifier across training and test datasets
     activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt",header=F)
     names(activity_labels) <- c("index","activity")
     activities_train <- read.table("./UCI HAR Dataset/train/y_train.txt") #list of activities for train_data
     activities_test <- read.table("./UCI HAR Dataset/test/y_test.txt") #list of activities for test_data
     activities_all <- rbind(activities_train,activities_test)
-    activities <- merge(activity_labels,activities_all,by.x = "index",by.y = "V1")
-    colnames(activities) <- c("activities_code","activities_name")
-    
-    subjects_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+    #Combine the subject identifier across training and test datasets
     subjects_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+    subjects_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
     subjects <- rbind(subjects_train,subjects_test)
     colnames(subjects) <- c("subjects")
-    
-    data3 <- cbind(subjects,activities,data2)
-    data4 <- data3[order(data3$subjects,data3$activities_code),]    
-    write.table(data4,"./getdataproject/data/data4.csv",append = FALSE,sep = ",",row.names=FALSE)
-    
-    data5 <- data4[ ,-2]
-    write.table(data5,"./getdataproject/data/data5.csv",append = FALSE,sep = ",",row.names=FALSE)     
+
+    #Add the subjects and activities identifiers to the mean and std dev data
+    data3 <- cbind(subjects,activities_all,data2)
+    data3.1 <- merge(activity_labels,data3,by.x = "index",by.y = "V1")
+    data4 <- data3.1[order(data3.1$subjects),]    
+
+    #remove the activity code column
+    data5 <- data4[,-1]
+
+    #reorder the columns to put the subject first
+    data6 <- data5[,c(2,1,3:68)]
 
 #do I need to worry about missing data? check for missing data
-    
-    #output a tidy data file (write data_clean to an output file)
 
 #change the names to be more descriptive
 
+#Sum the data by subject and activity
+data7 <- aggregate(.~subjects+activity,FUN = mean,data=data6)
 
+#Order data by subject ID
+finaldata <- data7[order(data7$subjects),]    
 
-
-
-#final <- dcast(data_consolidated,subjects + activities_name ~3:69,mean)
-data6 <- aggregate(.~subjects+activities_name,FUN = mean,data=data5)
-finaldata <- data6[order(data6$subjects,data6$activities_name),]    
-
+#output a tidy data file (write data_clean to an output file)
 write.table(finaldata,"./getdataproject/data/final.csv",append = FALSE,sep = ",",row.names=FALSE)     
